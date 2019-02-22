274b742b0abc11ae29d82fbd8a04c9cf68092f02
Ying Wen <wenyingying@hotmail.com>
2019-02-22 11:15:04 -0500

    Refactored the script, added plot function, updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Logistic-regression
-binary classification by using Python sklearn logistic regression 
+binary classification by using Python sklearn logistic regression
+
+## Usage:
+-  python logistic-regression-prediction.py insurance_data.csv
+-  python logistic-regression-prediction.py yourfile.csv
+
+## Functions:
+- read data from your local file (a default csv file will be created if there is no such file exists as what you specified as the argument from command line)
+- split to training data and test data in ratio of 9:1
+- create a logistic model by using the training data
+- test the model by using the test data
+- plot the input data and the model
--- logistic-regression-prediction.py
+++ logistic-regression-prediction.py
@@ -1,29 +1,56 @@
 """ a binary classfication and prediction example
 by using logistic regression (sklearn) """
 
+import os
+import sys
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklean.linear_model import LogisticRegression
 from sklearn.linear_model import LogisticRegression
 
 # read input data from program or from csv file
-d = {'age':[22,25,47,52,46,56,55,60,62,61,18,28,27,29,49,55,25,58,19,18,21,26,40,45,50,54,23],'bought_insurance':[0,0,1,0,1,1,0,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0]}
-df = pd.DataFrame(data=d)
-df.to_csv('insurance_data.csv', index=False, encoding='utf-8')
-df = pd.read_csv('insurance_data.csv')
+def load_data(path):
+    exists = os.path.isfile(path)
+    if not exists:
+        d = {'age':[22,25,47,52,46,56,55,60,62,61,18,28,27,29,49,55,25,58,19,18,21,26,40,45,50,54,23],'bought_insurance':[0,0,1,0,1,1,0,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0]}
+        df = pd.DataFrame(data=d)
+        df.to_csv(path, index=False, encoding='utf-8')
+        df = pd.read_csv(path)
+    else:
+        df = pd.read_csv(path)
+    return df
 
-# scatter plot of the input data
-plt.scatter(df.age, df.bought_insurance, marker='+', color='red')
-plt.show()
 
-# split into train and test dataset
-x_train, x_test, y_train, y_test = train_test_split(df[['age']],df.bought_insurance, test_size=0.1)
+def train_and_predict():
+    df = load_data(sys.argv[1])
 
-# train and predict
-model = LogisticRegression()
-model.fit(x_train,y_train)
-model.predict(x_test)
-model.score(x_test,y_test)
-model.predict_proba(x_test)
-model.predict([[70]])
+    # split into train and test dataset
+    x_train, x_test, y_train, y_test = train_test_split(df[['age']],df.bought_insurance, test_size=0.1)
+
+    # train and predict
+    model = LogisticRegression()
+    model.fit(x_train,y_train)
+    model.predict(x_test)
+    model.predict([[70]])
+
+    # model quality
+    model.score(x_test,y_test)
+    model.predict_proba(x_test)
+
+    def lr_model(x):
+        return 1 / (1 + np.exp(-x))
+    # plot the loss of the test data
+    loss = lr_model(x_test * model.coef_ + model.intercept_).values.ravel()
+    plt.plot(x_test, loss, color='black', linewidth=3)
+    sorted_input = df[['age']].sort_values(by=['age'])
+    whole_prediction = model.predict(sorted_input)
+    # plot the logistic model result
+    plt.plot(sorted_input, whole_prediction, color='red', linewidth=3 )
+    # scatter plot of the input data
+    plt.scatter(df.age, df.bought_insurance, marker='+', color='blue')
+    plt.show()
+
+
+if __name__=="__main__":
+    train_and_predict()
